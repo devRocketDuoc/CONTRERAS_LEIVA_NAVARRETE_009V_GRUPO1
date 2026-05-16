660957c10b5adfd0e28a6f9945df5e17379f43bf
DESKTOP-JU5OT3G\albir <bas.contrerasa@duocuc.cl>
2026-05-16 14:59:51 -0400

    fix implementation notification service

--- air-fly-trip/notification-service/src/main/java/cl/duoc/airflytrip/notifications/dtos/request/CreateNotificationRequest.java
+++ air-fly-trip/notification-service/src/main/java/cl/duoc/airflytrip/notifications/dtos/request/CreateNotificationRequest.java
@@ -1,5 +1,25 @@
 package cl.duoc.airflytrip.notifications.dtos.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateNotificationRequest {
 
+    @NotNull(message = "User id is required")
+    private Long userId;
+
+    @NotBlank(message = "Notification type is required")
+    private String type;
+
+    @NotBlank(message = "Notification title is required")
+    private String title;
+
+    @NotBlank(message = "Notification message is required")
+    private String message;
+
+    private String status;
 }
